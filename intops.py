41f4051f88e45b69d6bfce9f377330f5faa4806b
unknown <Fang_Wenqi@outlook.com>
2023-10-31 10:40:23 +0100

    Haha

---
+++ intops.py
@@ -0,0 +1,9 @@
+a=int(input("the first number:"))
+b=int(input("the second number:"))
+
+print(a,"+",b,"=",a+b)
+print(a,"-",b,"=",a-b)
+print(a,"*",b,"=",a*b)
+print(a,"//",b,"=",a//b)
+print(a,"%",b,"=",a%b)
+print(a,"**",b,"=",a**b)
